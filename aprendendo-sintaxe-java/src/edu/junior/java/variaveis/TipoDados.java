88e0fb1a85501ac2ea4d8f150ad79276aea84ced
JuniorMelo <juniormelo.dev@gmail.com>
2023-02-18 20:12:55 -0300

    Adicionando principais tipos de dados em java

---
+++ aprendendo-sintaxe-java/src/edu/junior/java/variaveis/TipoDados.java
@@ -0,0 +1,21 @@
+package edu.junior.java.variaveis;
+
+public class TipoDados {
+    public static void main(String[] args) {
+
+        // criando tipos de dados básicos
+        byte idade = 33;
+        short ano = 2023;
+        int cep = 61700000;
+        long cpf = 14015612348L;
+        float pi = 3.14F;
+        double salario = 1302.55;
+
+        System.out.println(idade);
+        System.out.println(ano);
+        System.out.println(cep);
+        System.out.println(cpf);
+        System.out.println(pi);
+        System.out.println(salario);
+    }
+}

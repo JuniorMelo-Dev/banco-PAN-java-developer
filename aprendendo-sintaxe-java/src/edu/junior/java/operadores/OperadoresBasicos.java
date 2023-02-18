0f6da6a213b82dec94b18cba7802059e9e689548
JuniorMelo <juniormelo.dev@gmail.com>
2023-02-18 20:46:29 -0300

    Aula sobre operadores atribuição e aritméticos

---
+++ aprendendo-sintaxe-java/src/edu/junior/java/operadores/OperadoresBasicos.java
@@ -0,0 +1,16 @@
+package edu.junior.java.operadores;
+
+public class OperadoresBasicos {
+
+    // Atribuição (=)
+    String nome = "JuniorMelo";
+    //Date dataNascimento = new Date(); //atribuindo novo objeto
+
+    // Aritmético
+    double soma = 10.5 + 15.8;
+    int subtracao = 2005 - 56;
+    int multiplicacao = 15 * 9;
+    int divisao = 91 / 7;
+    int modulo = 5 % 2;
+    double resultado = (10 * 8) + (15 / 6);
+}

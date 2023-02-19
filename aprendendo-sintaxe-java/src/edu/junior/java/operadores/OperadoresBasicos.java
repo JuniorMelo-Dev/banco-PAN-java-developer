2865480892497b7a4f550528c1a65646dc5ed2a7
JuniorMelo <juniormelo.dev@gmail.com>
2023-02-19 13:41:43 -0300

    Aula sobre operadores unários e de negação

--- aprendendo-sintaxe-java/src/edu/junior/java/operadores/OperadoresBasicos.java
+++ aprendendo-sintaxe-java/src/edu/junior/java/operadores/OperadoresBasicos.java
@@ -1,16 +1,36 @@
 package edu.junior.java.operadores;
 
+import java.util.function.DoubleToIntFunction;
+
 public class OperadoresBasicos {
+    public static void main(String[] args) {
+        // Atribuição (=)
+        String nome = "JuniorMelo";
+        System.out.println(nome);
+        // Date dataNascimento = new Date(); //atribuindo novo objeto
 
-    // Atribuição (=)
-    String nome = "JuniorMelo";
-    //Date dataNascimento = new Date(); //atribuindo novo objeto
+        // Aritmético
+        double soma = 10.5 + 15.8;
+        int subtracao = 2005 - 56;
+        int multiplicacao = 15 * 9;
+        int divisao = 91 / 7;
+        int modulo = 5 % 2;
+        double resultado = (10 * 8) + (15 / 6);
 
-    // Aritmético
-    double soma = 10.5 + 15.8;
-    int subtracao = 2005 - 56;
-    int multiplicacao = 15 * 9;
-    int divisao = 91 / 7;
-    int modulo = 5 % 2;
-    double resultado = (10 * 8) + (15 / 6);
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+        System.out.println(modulo);
+        System.out.println(resultado);
+
+        // Unário
+        int numero = 10;
+        boolean var = true;
+
+        System.out.println(++numero);
+        System.out.println(--numero);
+        System.out.println(!var);
+    }
+
 }

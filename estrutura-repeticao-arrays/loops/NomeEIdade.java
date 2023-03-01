531717cdb0db714d9e6e41b49fc59a4b1bd3c2a0
JuniorMelo <juniormelo.dev@gmail.com>
2023-03-01 15:18:27 -0300

    Inserindo comentário

--- estrutura-repeticao-arrays/loops/NomeEIdade.java
+++ estrutura-repeticao-arrays/loops/NomeEIdade.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
 
+/*
+Faça um programa que leia conjuntos de dois valores,
+o primeiro representando o nome do aluno e o segundo representando a sua idade.
+(Pare o programa inserindo o valor 0 no campo nome)
+*/
+
 public class NomeEIdade {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
